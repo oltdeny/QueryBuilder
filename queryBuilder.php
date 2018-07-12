698cad3c4e65f43fc7f4ad9e964dff2d823d66c1
rtd21 <36414175+rtd21@users.noreply.github.com>
2018-07-12 11:03:52 +0400

    Ready methods SELECT, INSERT, UPDATE with different implementation.

--- queryBuilder.php
+++ queryBuilder.php
@@ -17,18 +17,18 @@ class queryBuilder{
         self::$mysqli->query("SET NAMES 'utf8'");
     }
 
-    public static function dbClose(){
+    public function dbClose(){
         self::$mysqli->close();
     }
 
-    public static function select($columns){
+    public function select($columns){
         if(gettype($columns) != "array"){
             return false;
         }
         self::$select = "SELECT ".implode(",", $columns);
 
     }
-    public static function from($table){
+    public function from($table){
         if(gettype($table) != "string"){
             return false;
         }
@@ -37,7 +37,7 @@ class queryBuilder{
     /**
      * $cases - ассоциативный массив
      */
-    public static function where($cases){
+    public function where($cases){
         if(gettype($cases) != "array"){
             return false;
         }
@@ -49,43 +49,61 @@ class queryBuilder{
             }
             self::$where .= $key.'='.$case;
         }
-
-    }
-    public static function insert($mysqli, $table, $columns){
-        $insert = "INSERT INTO ".$table;
-        $keys = "(";
-        foreach ($columns as $key => $column) {
-            if ($key < count($columns)-1){
-                $keys .= $key. ',';
-                continue;
-            }
-            $keys .= $key.")";
-        }
-        $insert .= $keys."VALUES";
-        $values = "(";
-        foreach ($columns as $key => $column) {
-            if ($key < count($columns)-1){
-                $values .= $column . ',';
-                continue;
-            }
-            $values .= $column.")";
-        }
-        $insert .= $values;
-        return $mysqli->query($insert);
     }
 
-    public static function update($mysqli, $table, $columns, $id){
-        $update = "UPDATE ".$table." SET ";
-        $values = "";
-        foreach ($columns as $key => $column) {
-            if ($key < count($columns)-1){
-                $values .= $key . '=' . $column . ",";
-                continue;
-            }
-            $values .= $key . '=' . $column;
-        }
-        $update .= $values." WHERE ".$table.".`id` = ".$id;
-        return $mysqli->query($update);
+    public function execute(){
+        $query = self::$select.self::$from.self::$where;
+        return self::$mysqli->query($query);
     }
+
+    public function insert($table, $columns){
+        if(gettype($table) == "string" && gettype($columns) == "array"){
+            $insert = "INSERT INTO ".$table;
+            $keys = "(";
+            foreach ($columns as $key => $column) {
+                if ($key < count($columns)-1){
+                    $keys .= $key. ',';
+                    continue;
+                }
+                $keys .= $key.")";
+            }
+            $insert .= $keys."VALUES";
+            $values = "(";
+            foreach ($columns as $key => $column) {
+                if ($key < count($columns)-1){
+                    $values .= $column . ',';
+                    continue;
+                }
+                $values .= $column.")";
+            }
+            $insert .= $values;
+            return self::$mysqli->query($insert);
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function update($table, $columns, $id){
+        if(gettype($table) == "string" && gettype($columns) == "array" && gettype($id) == "integer"){
+            $update = "UPDATE ".$table." SET ";
+            $values = "";
+            foreach ($columns as $key => $column) {
+                if ($key < count($columns)-1){
+                    $values .= $key . '=' . $column . ",";
+                    continue;
+                }
+                $values .= $key . '=' . $column;
+            }
+            $update .= $values." WHERE ".$table.".`id` = ".$id;
+            return self::$mysqli->query($update);
+        }
+        else{
+            return false;
+        }
+
+    }
+
+
 
 }
